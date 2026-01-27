94deec4954be458fedec97b327107af41ccba01a
DERANJANAHARY <deranjanahary@gmail.com>
2026-01-27 03:27:18 +0300

    W3

--- scripts/place.js
+++ scripts/place.js
@@ -1,11 +1,9 @@
-
 // FOOTER DATES
 document.getElementById("currentyear").textContent =
   new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
   document.lastModified;
-
 
 // STATIC WEATHER VALUES
 const temperature = 10; // °C
@@ -14,8 +12,7 @@ const windSpeed = 5; // km/h
 document.getElementById("temp").textContent = temperature;
 document.getElementById("wind").textContent = windSpeed;
 
-
-// WIND CHILL FUNCTION
+// WIND CHILL FUNCTION (single return line)
 function calculateWindChill(temp, wind) {
   return (temp <= 10 && wind > 4.8)
     ? (13.12 + 0.6215 * temp - 11.37 * wind ** 0.16 + 0.3965 * temp * wind ** 0.16).toFixed(1)
