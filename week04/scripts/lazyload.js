282b4ba0e1693daf5fa3916c340767bef41f82a6
DERANJANAHARY <deranjanahary@gmail.com>
2026-02-01 17:15:14 +0300

    W4 practice

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,6 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+// Get the last modified date of the document
+document.getElementById("lastModified").innerHTML = document.lastModified;
